a4f13026aa5a682fbb7986845e18736ff5f19cea
hoge1e3 <hoge1e3@gmail.com>
2025-06-21 12:30:06 +0900

    traverseTree

--- test/test-git.ts
+++ test/test-git.ts
@@ -2,14 +2,16 @@ import {Repo} from "../src/git.js";
 import * as assert from "assert";
 export async function main(){
     const repo=new Repo(".git");
-    const obj=await repo.readObject("00d6602b2832d060ad2a2f26c4b5bd957aa2dde8");
-    console.log(obj.type, obj.content);
+    //const obj=await repo.readObject("00d6602b2832d060ad2a2f26c4b5bd957aa2dde8");
+    //console.log(obj.type, obj.content);
     const head=await repo.readHead("main");
     console.log(head);
     const commit=await repo.readCommit(head);
     console.log(commit);
     const tree=await repo.readTree(commit.tree);
     console.log(tree);
-    
+    for await (let e of repo.traverseTree(tree)) {
+        console.log(e.path, e.hash);
+    }
 }    
 main();
