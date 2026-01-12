49297aee4b6cf44e839aebaea451b69cb794ab2b
hoge1e3 <hoge1e3@gmail.com>
2026-01-12 15:45:40 +0900

    test passed??

--- node/test/test2.ts
+++ node/test/test2.ts
@@ -4,8 +4,8 @@ import * as os from "os";
 import * as assert from "assert";
 
 import { Repo } from "../src/git.js";
-import { commit, clone, sync, init } from "../src/cmd.js";
-import { asFilePath, asBranchName, asLocalRef, isHash } from "../src/types.js";
+import { commit, clone, sync, init, syncWithRetry } from "../src/cmd.js";
+import { asFilePath, asBranchName, asLocalRef, isHash, FilePath, Hash } from "../src/types.js";
 import { GIT_DIR_NAME, Sync } from "../src/sync.js";
 
 const mainBranch = asBranchName("main");
@@ -23,6 +23,13 @@ async function initRepo(dir: string): Promise<Sync> {
   const sync = new Sync(asFilePath(path.join(dir, GIT_DIR_NAME)));
   return sync;
 }
+async function getCurrentTreeHash(dir: FilePath): Promise<Hash> {
+  //const sync = new Sync(asFilePath(path.join(dir, GIT_DIR_NAME)));
+  const repo = new Repo(asFilePath(path.join(dir, GIT_DIR_NAME)));
+  const commitHash=(await repo.readHead(asLocalRef(await repo.getCurrentBranchName())) )!;
+  const commitObj=await repo.readCommit(commitHash);
+  return commitObj.tree;
+}
 
 export async function test_scenario_basic_sync() {
   const testdir="../cotest";
@@ -39,16 +46,17 @@ export async function test_scenario_basic_sync() {
 
   // コミット
   const firstCommit = await commit(originDir);
-  console.log("first commit",firstCommit);
-  assert.ok(isHash(firstCommit), "initial commit should exist");
-
+  const firstTree = await getCurrentTreeHash(originDir);
+  console.log("firstTree",firstTree);
+  assert.ok(isHash(firstTree), "initial commit should exist");
+  assert.equal(firstTree,"e30b5202a91c72788d1ce94a3918edb647d2f48c","initial tree hash should match");
   // sync
   await sync(originDir,"saveHashedRemote");
   
   //
   // 3) 別フォルダに clone
   //
-  const cloneDir = fs.mkdtempSync(path.join(testdir, "clone-"));
+  const cloneDir = asFilePath(fs.mkdtempSync(path.join(testdir, "clone-")));
   cleanups.push(()=>fs.rmSync(cloneDir, { recursive: true, force: true }));
 
   const conf = await originRepo.readConfig();
@@ -70,8 +78,10 @@ export async function test_scenario_basic_sync() {
   write(path.join(cloneDir, "c.txt"), "new file");
 
   const secondCommit = await commit(cloneDir);
-  console.log("secondCommit",secondCommit);
-  assert.ok(isHash(secondCommit), "second commit should exist");
+  const secondTree = await getCurrentTreeHash(cloneDir);
+  console.log("secondTree",secondTree);
+  //assert.ok(isHash(secondTree), "second commit should exist");
+  assert.equal(secondTree,"8c2151c1341ae06d3cd449a34ad26c96d2bfe0f2","second tree hash should match");
 
   const sync_clone=new Sync(asFilePath(path.join(cloneDir,GIT_DIR_NAME)));
   assert.ok( await sync_clone.hasRemoteHead(mainBranch), " has remote head should be set" );
@@ -89,6 +99,44 @@ export async function test_scenario_basic_sync() {
     fs.readFileSync(path.join(originDir, "c.txt"), "utf8"),
     "new file"
   );
+  await test_scenario_merge(originDir, cloneDir);
+}
+export async function test_scenario_merge(originDir:FilePath, cloneDir:FilePath) { 
+  write(path.join(originDir, "a.txt"), "from origin");
+  write(path.join(cloneDir, "b.txt"), "from cloned");
+  assert.equal(
+    fs.readFileSync(path.join(originDir, "a.txt"), "utf8"),
+    "from origin"
+  );
+  assert.equal(
+    fs.readFileSync(path.join(cloneDir, "b.txt"), "utf8"),
+    "from cloned"   
+  );
+  const thirdCommit_o = await syncWithRetry(originDir, "saveHashedRemote");
+  const thirdTree_o = await getCurrentTreeHash(originDir);
+  console.log("thirdTree_o",thirdTree_o);
+  assert.equal(thirdTree_o,"022aea6b51dba69b72294b89b3b6ebb506441d78","third tree(of origin) hash should match");
+  const thirdCommit_c = await syncWithRetry(cloneDir, "saveHashedRemote");
+  const thirdTree_c = await getCurrentTreeHash(cloneDir);
+  assert.equal(thirdTree_c,"81add420c8ea9308bc16fc510790489fa408097c","third tree(of clone) hash should match");
+  console.log("thirdTree_c",thirdTree_c);
+
+  assert.equal(
+    fs.readFileSync(path.join(cloneDir, "a.txt"), "utf8"),
+    "from origin"
+  );  
+  
+
+  const fourthCommit_o = await syncWithRetry(originDir, "saveHashedRemote");
+  const fourthTree_o = await getCurrentTreeHash(originDir);
+  console.log("fourthTree_o",fourthTree_o);
+  assert.equal(fourthTree_o,thirdTree_c,"third tree(of object) and 4th hash should match");
+  assert.equal(
+    fs.readFileSync(path.join(originDir, "b.txt"), "utf8"),
+    "from cloned"   
+  );
+
+
 }
 export async function main(){
   await test_scenario_basic_sync();
